b2144b1f54368dbf983181dc628059aa4cdee130
Pratik-Guha <142882560+Pratik-Guha@users.noreply.github.com>
2023-10-08 15:11:56 +0530

    Update AllPost.jsx

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -13,9 +13,9 @@ const AllPost = () => {
   return (
     <div className='w-full py-8'>
         <Container>
-            <div className=' flex flex-wrap'>
+            <div className=' flex flex-wrap flex-col justify-center items-center sm:flex-row'>
                 {posts.map((post)=>(
-                    <div key={post.$id} className=' p-2 w-1/4'>
+                    <div key={post.$id} className='p-2 w-full md:w-1/4'>
                         <PostCard {...post}/>
                     </div>
                 ))}
